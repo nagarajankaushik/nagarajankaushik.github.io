8e798c1c6e2b809c5fb607236524d31ed8714126
Kaushik Nagarajan <nagarajankaushik@github.com>
2020-10-01 01:02:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const tl = gsap.timeline({
+    defaults: {
+        ease: "power1.out"
+    }
+});
+
+tl.to(".text", {y:"0%", duration:1, stagger:0.5});
+tl.to(".slider", {y:"-100%", duration:1.5, delay: 2.5});
+// tl.to(".intro", {y:"-100%", duration:1.5, delay: 2.5});
+tl.to(".intro", {y:"-100%", duration:1}, "-=1.5");
